70f5b51dfa3aefb88b3990d6e670b8f1c06d4564
Timo Rantalainen <tjrantal@gmail.com>
2015-07-16 17:35:51 +1000

    Subregions selection and visualization seems to work

--- src/edu/deakin/timo/ROISubregions.java
+++ src/edu/deakin/timo/ROISubregions.java
@@ -42,17 +42,36 @@ public class ROISubregions implements PlugIn {
 	private Preferences preferences;		/**Saving the default file path*/
 	private final String keySP = "SP";
 	private String savePath;
-	RoiManager rMan;
-
 
 	/**Implement the PlugIn interface*/
     public void run(String arg) {
 		imw = WindowManager.getCurrentWindow();
 		canvas = imw.getCanvas();
         imp = WindowManager.getCurrentImage();
-		//IJ.log(imp.getStack().getShortSliceLabel(imp.getSlice()));
+		
+		
+		//Get settings from the ROISettings
+		/*Get save path*/
+		Frame[] frames = Frame.getFrames();
+		int fr = 0;
+		while (fr < frames.length && frames[fr].getTitle() != "ROISettings"){
+			++fr;
+		}
+		//If ROISettings have not been opened, create new
+		ROISettings rSettings;
+		if (fr>= frames.length || frames[fr].getTitle() != "ROISettings"){
+			rSettings = new ROISettings();
+		}else{
+			if (frames[fr] instanceof edu.deakin.timo.utils.ROISettings){
+				rSettings = (ROISettings) frames[fr];
+			}else{
+				rSettings = new ROISettings();	//Should never get here!
+			}
+		}
+		rSettings.setVisible(true);
+		String settings[] = rSettings.getSettings();
 
-		int[] subDivisions = new int[]{5,1};	/*Get this from a menu, width wise divisions, height wise*/
+		int[] subDivisions = new int[]{Integer.parseInt(settings[1]),Integer.parseInt(settings[0])};	/*Get this from a menu, width wise divisions (columns), height wise (rows)*/
 		
 		/*Check that an image was open*/
 		if (imp == null) {
@@ -65,49 +84,32 @@ public class ROISubregions implements PlugIn {
 			return;
 		}
 		
-		
-		
 		/*Get image size and stack depth*/
 		int width = imp.getWidth();
 		int height = imp.getHeight();
 		depth = imp.getStackSize();
 		int currentSlice =imp.getSlice();
-		
-		/**Pop up Roi Manager*/
-		if (RoiManager.getInstance() == null){
-			rMan = new RoiManager();
-		}else{
-			rMan = RoiManager.getInstance();
-		}
-		/**Add the ROI to roimanager*/
-		//Go through 8 consecutive slices
+
 		ImagePlus visualIP		= null;
 		SubRegions subRegions	= null;
-		for (int i = 0; i<8;++i){
-			imp.setSlice(currentSlice);
-			rMan.add(imp,imp.getRoi(),imp.getSlice());
-			//IJ.log("stack depth "+depth+" current slice "+currentSlice);
-			/**Get the visualization stack*/
-			visualIP = getVisualizationStack(imp);
-			
-			/**Get ROI mask for the current ROI*/
-			byte[] roiMask = getRoiMask(imp);
-			/**Get the mask pixel coordinates, calculate the rotation angle for the ROI, and get the rotated coordinates*/
-			PixelCoordinates pixelCoordinates = new PixelCoordinates(roiMask,width,height);
-			
-			subRegions = new SubRegions(imp,pixelCoordinates,subDivisions);
-			subRegions.printResults();	//Print the results to a TextPanel
-			/*Color the subregions to visualize the division*/
-			visualizeRegions(visualIP,subRegions,subDivisions,pixelCoordinates);
-			/*Re-activate the original stack*/
-			WindowManager.toFront(imw);
-			WindowManager.setWindow(imw); 
-			WindowManager.setCurrentWindow(imw); 
-			++currentSlice;
-		}
-		if (visualIP != null && subRegions != null){
-			subRegions.saveResults(savePath,imp,visualIP,rMan);	//Save the results to disk		
-		}
+
+		/**Get the visualization stack*/
+		visualIP = getVisualizationSlice(imp);
+		
+		/**Get ROI mask for the current ROI*/
+		byte[] roiMask = getRoiMask(imp);
+		/**Get the mask pixel coordinates, calculate the rotation angle for the ROI, and get the rotated coordinates*/
+		PixelCoordinates pixelCoordinates = new PixelCoordinates(roiMask,width,height);
+		
+		subRegions = new SubRegions(imp,pixelCoordinates,subDivisions);
+		subRegions.printResults();	//Print the results to a TextPanel
+		/*Color the subregions to visualize the division*/
+		visualizeRegions(visualIP,subRegions,subDivisions,pixelCoordinates);
+		/*Re-activate the original stack*/
+		WindowManager.toFront(imw);
+		WindowManager.setWindow(imw); 
+		WindowManager.setCurrentWindow(imw); 
+
 	}
 	
 	private void visualizeRegions(ImagePlus visualIP,SubRegions subRegions,int[] subDivisions,PixelCoordinates pc){
@@ -120,26 +122,11 @@ public class ROISubregions implements PlugIn {
 				rgb[c] = (value >>(c*8))& 0XFF;
 			}
 			//Come up with a proper colour palette selection at some point...
-			switch (regionIndices[0][i]+regionIndices[1][i]*subDivisions[0]){
-				case 0:
-					rgb[1] = 0;
-					break;
-				case 1:
-					rgb[0] = 0;
-					rgb[1] = 0;
-					break;
-				case 2:
-					rgb[0] = 0;
-					rgb[2] = 0;
-					break;
-				case 3:
-					rgb[1] = 0;
-					rgb[2] = 0;
-					break;
-				case 4:
-					rgb[2] = 0;
-					break;
-			}
+			rgb[0] = (int) (((double) rgb[0])*((double) ((regionIndices[0][i]+regionIndices[1][i]*subDivisions[0])))/((double)(subDivisions[0]*subDivisions[1])));
+			rgb[1] = (int) (((double) rgb[1])*((double) ((subDivisions[0]*subDivisions[1])-(regionIndices[0][i]+regionIndices[1][i]*subDivisions[0])))/((double)(subDivisions[0]*subDivisions[1])));
+			rgb[2] = (int) (((double) rgb[1])*((double) ((subDivisions[0]*subDivisions[1])-(regionIndices[0][i]*subDivisions[1]+regionIndices[1][i])))/((double)(subDivisions[0]*subDivisions[1])));
+			//rgb[1] = (int) ((double) rgb[1])*(((double)(subDivisions[0]*subdivisions[1]))-((double) (regionIndices[0][i]+regionIndices[1][i]*subDivisions[0])))/(((double)(subDivisions[0]*subdivisions[1]);
+
 			visualIP.getProcessor().setColor(new Color(rgb[2],rgb[1],rgb[0]));
 			visualIP.getProcessor().drawPixel((int)pc.coordinates[i][0],(int)pc.coordinates[i][1]);
 		}
@@ -186,96 +173,22 @@ public class ROISubregions implements PlugIn {
 	}
 
 	/**
-		Duplicate the current stack, or if it has already been duplicated get the duplicate stack, and set the slice to the ROI slice
+		Duplicate the current image, and set color model to RGB
 		@param imp the stack with the current ROI
 	*/
-	private ImagePlus getVisualizationStack(ImagePlus imp){
+	private ImagePlus getVisualizationSlice(ImagePlus imp){
 		/**Duplicate the image stack to be used for visualization*/
 		int width = imp.getWidth();
 		int height = imp.getHeight();
-		int depth = imp.getStackSize();
-		int currentSlice = imp.getSlice();
 		String stackName = imp.getTitle();
 		String visualName = stackName+" visualization";
 		Window vsw = WindowManager.getWindow(visualName);
 		ImagePlus visualIP;
-		if (vsw == null){
-			//IJ.log("Didn't find visual stack");
-			/*Create a visualization stack, duplicate the original stack*/
-			ImageStack visualizationStack = new ImageStack(width,height);
-			//IJ.log("Stack created");
-			for (int i = 1;i<=depth;++i){
-				imp.setSlice(i);
-				//IJ.log("Slice changed");
-				short[] slicePixels = Arrays.copyOf((short[]) imp.getProcessor().getPixels(),((short[])imp.getProcessor().getPixels()).length);
-				//IJ.log("Copied pixels "+slicePixels.length);
-				//visualizationStack.addSlice(imp.duplicate().getProcessor());
-				visualizationStack.addSlice(null,slicePixels);
-				//IJ.log("Added slice "+i);
-			}
-			//IJ.log("Set imp to currentSlice");
-			imp.setSlice(currentSlice);
-			
-			//visualizationStack =imp.getImageStack().duplicate();
-			//IJ.log("Duplicated stack");
-			//visualIP = imp.duplicate(); //new ImagePlus(visualName,visualizationStack);
-			visualIP = new ImagePlus(visualName,visualizationStack);
-			//visualIP.setTitle(visualName);
-			//IJ.log("Got imagePlus for vstack");
-			new ImageConverter(visualIP).convertToRGB();	//Convert the stack to RGB for visualization
-			
-			/**Define save path*/
-			preferences = Preferences.userRoot().node(this.getClass().getName());
-			try{
-				savePath = preferences.get(keySP,new File( "." ).getCanonicalPath()); /*Use current working directory as
-			default*/
-			}catch (IOException ex){
-				System.out.println(ex);
-				savePath = ".";
-			}
-			/*Instantiate fileChooser*/
-			fileChooser = new JFileChooser(savePath);							/*Implements the file chooser*/
-			//fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);	/*Limit to choosing files*/
-			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			int returnVal = fileChooser.showOpenDialog(WindowManager.getActiveWindow());
-             if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-				try{
-					if (file.isDirectory()){
-						preferences.put(keySP,file.getCanonicalPath());
-					}else{
-						preferences.put(keySP,(new File(file.getParent())).getCanonicalPath());
-					}
-					System.out.println("Save path set "+preferences.get(keySP,"."));
-				}catch (IOException ex){
-					System.out.println(ex);
-					savePath = ".";
-				}
-            } else {
-                System.out.println("Cancelled file dialog");
-            }
-		}else{
-			//IJ.log("Found visual stack");
-			WindowManager.setWindow(vsw);
-			if (vsw instanceof ImageWindow){
-				//IJ.log("visual stack instanceof ImageWindow");
-				WindowManager.setCurrentWindow((ImageWindow) vsw);
-			}
-			
-			//WindowManager.setCurrentWindow(WindowManager.getActiveWindow());
-			visualIP =   WindowManager.getCurrentImage();
-			//visualizationStack = visualIP.getImageStack();
-			/**Define save path, read the value from preferences*/
-			preferences = Preferences.userRoot().node(this.getClass().getName());
-			try{
-				savePath = preferences.get(keySP,new File( "." ).getCanonicalPath()); /*Use current working directory as
-			default*/
-			}catch (IOException ex){
-				System.out.println(ex);
-				savePath = ".";
-			}
-		}
-		visualIP.setSlice(currentSlice);
+		short[] slicePixels = Arrays.copyOf((short[]) imp.getProcessor().getPixels(),((short[])imp.getProcessor().getPixels()).length);
+		ShortProcessor tp = new ShortProcessor(width,height);
+		tp.setPixels(slicePixels);
+		visualIP = new ImagePlus(visualName,tp);
+		new ImageConverter(visualIP).convertToRGB();	//Convert the stack to RGB for visualization
 		return visualIP;
 	}
 }
